5d7d162e9469f346d4f650b2d589b1febb61695f
EchoHunter <48270082+EchoHunter@users.noreply.github.com>
2020-03-26 17:53:48 -0700

    Update Intor_to_Constructors.java

--- src/Intro_to_Constructors/Intor_to_Constructors.java
+++ src/Intro_to_Constructors/Intor_to_Constructors.java
@@ -1,5 +1,22 @@
 package Intro_to_Constructors;
 
 public class Intor_to_Constructors {
-
+	
+	
+	
+	
+	
+	public class Platypus {
+	    private String name;
+	    void sayHi(){
+	        System.out.println("The platypus " + name + " is smarter than your average platypus.");
+	    }
+	    
+	}
+	public class Runner{
+		Platypus p = new Platypus();
+      p.
+	}
 }
+
+
