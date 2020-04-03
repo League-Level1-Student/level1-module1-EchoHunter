43624657769a7549cccc40e12eee14176245a93e
EchoHunter <48270082+EchoHunter@users.noreply.github.com>
2020-04-02 18:32:25 -0700

    :b: 2
    finished safecrack and started Duck

--- src/_05_vault/safeCrack.java
+++ src/_05_vault/safeCrack.java
@@ -5,28 +5,37 @@ import java.util.Random;
 class vault {
 	public int r = new Random().nextInt(1000001);
 	public	int t ;
-	public	int secretCode = r ;
-	void trycode(int t) {
+	public 	int secretCode = r ;
+	boolean trycode(int t) {
 		if (t == (secretCode)) {
 			System.out.println("true");
+			return true;
 		}
 		else {
 			System.out.println("false");
+			return false;
 		}
+	
 	}
 }
 public class safeCrack {
-
+static jamesBond j = new jamesBond();
+public static void main(String[] args) {
 	
+
+j.crackSafe();
+}	
 }
 class jamesBond {
 	vault v = new vault();
 	void crackSafe() {
 		for (int i = 0; i < 1000001; i++) {
-		
-			if (i== secretCode) {
-				
-			}
+		if( v.trycode(i)==  true) {
+			System.out.println(i);
+			break;
+		}
+	
+			
 		}
 	}
 	
---
+++ src/_06_duck/duck.java
@@ -0,0 +1,7 @@
+package _06_duck;
+
+public class duck {
+int numberOfFriends;
+String favoriteFood;
+
+}
